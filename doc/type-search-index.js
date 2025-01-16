ff8b361e128a2f6eee2497e64fbd5169bc63fca1
annemcdesign <ae@mccubbin.org.uk>
2025-01-16 16:34:07 +0000

    doc folder

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"gamePackage","l":"Card"},{"p":"gamePackage","l":"Computer"},{"p":"gamePackage","l":"Game"},{"p":"gamePackage","l":"GameTest"},{"p":"gamePackage","l":"Human"},{"p":"gamePackage","l":"Main"},{"p":"gamePackage","l":"Player"},{"p":"gamePackage","l":"UserInterface"}];updateSearchResults();
